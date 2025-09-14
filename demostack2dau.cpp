4b682ab29a0fda328fb5cf1bd014f3f7aac9ea91
mactoan0008-cell <mactoan0008@gmail.com>
2025-09-14 14:09:20 +0700

    Update demostack2dau.cpp

--- demostack2dau.cpp
+++ demostack2dau.cpp
@@ -33,7 +33,7 @@ public:
     void pushLeft(int x) {
         if (full()) {
             setColor(12);
-            cout << "Error: Stack full!\n";
+            gotoxy(0, 15); cout << "Error: Stack full!          ";
             setColor(7);
             Sleep(1000);
             return;
@@ -44,7 +44,7 @@ public:
     void pushRight(int x) {
         if (full()) {
             setColor(12);
-            cout << "Error: Stack full!\n";
+            gotoxy(0, 15); cout << "Error: Stack full!          ";
             setColor(7);
             Sleep(1000);
             return;
@@ -55,25 +55,23 @@ public:
     void popLeft() {
         if (top1 < 0) {
             setColor(12);
-            cout << "Error: Left stack empty!\n";
+            gotoxy(0, 15); cout << "Error: Left stack empty!    ";
             setColor(7);
             Sleep(1000);
             return;
         }
-        cout << "Pop left: " << arr[top1--] << endl;
-        system("pause");
+        gotoxy(0, 15); cout << "Pop left: " << arr[top1--] << "           ";
     }
 
     void popRight() {
         if (top2 >= MAX) {
             setColor(12);
-            cout << "Error: Right stack empty!\n";
+            gotoxy(0, 15); cout << "Error: Right stack empty!   ";
             setColor(7);
             Sleep(1000);
             return;
         }
-        cout << "Pop right: " << arr[top2++] << endl;
-        system("pause");
+        gotoxy(0, 15); cout << "Pop right: " << arr[top2++] << "          ";
     }
 
     void drawUI() {
@@ -83,11 +81,12 @@ public:
         int originX = 10;
         int originY = 5;
 
+        // Tiêu đ? stack
         gotoxy(originX + 4, originY - 1); cout << "S1";
         gotoxy(originX + 24, originY - 1); cout << "S2";
 
-        gotoxy(originX, originY);
-        cout << "+";
+        // V? khung
+        gotoxy(originX, originY); cout << "+";
         for (int i = 0; i < MAX; i++) {
             if (i < 5) setColor(9);
             else setColor(12);
@@ -95,18 +94,19 @@ public:
         }
         setColor(7);
 
-        gotoxy(originX, originY + 1);
-        cout << "|";
+        // V? giá tr?
+        gotoxy(originX, originY + 1); cout << "|";
         for (int i = 0; i < MAX; i++) {
             if (i < 5) setColor(9);
             else setColor(12);
+
             if (i <= top1 || i >= top2) cout << setw(3) << arr[i] << "|";
             else cout << "   |";
         }
         setColor(7);
 
-        gotoxy(originX, originY + 2);
-        cout << "+";
+        // V? đáy
+        gotoxy(originX, originY + 2); cout << "+";
         for (int i = 0; i < MAX; i++) {
             if (i < 5) setColor(9);
             else setColor(12);
@@ -114,20 +114,23 @@ public:
         }
         setColor(7);
 
+        // M?i tên pHead/pTail
         int arrowY = originY + 3;
         int labelY = originY + 4;
 
-        int headX = (top1 == -1) ? originX - 2 : originX + 2 + top1 * 4;
+        int headX = originX + 2 + (top1 + 1) * 4;
         setColor(9);
         gotoxy(headX, arrowY); cout << "^";
         gotoxy(headX - 2, labelY); cout << "pHead";
 
-        int tailX = (top2 == MAX) ? originX + 2 + MAX * 4 : originX + 2 + top2 * 4;
+        int tailX = originX + 2 + (top2 - 1) * 4;
         setColor(12);
         gotoxy(tailX, arrowY); cout << "^";
         gotoxy(tailX - 2, labelY); cout << "pTail";
 
         setColor(7);
+
+        // Menu
         gotoxy(0, labelY + 3);
         cout << "MENU:\n";
         cout << "1. Push left\n2. Push right\n3. Pop left\n4. Pop right\n5. Exit\n";
